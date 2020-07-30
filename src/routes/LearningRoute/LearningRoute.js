0a80862c0af51e2a00e24e119fed5ec4a11fe166
Brian Charles <bcharles41078@gmail.com>
2020-07-30 09:11:20 -0500

    day 4

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LanguageService from '../../services/language-service';
+import './LearningRoute.css'
 
 class LearningRoute extends Component {
   constructor(props) {
@@ -41,23 +42,27 @@ class LearningRoute extends Component {
         <h2>
           Translate the word:
         </h2>
+        <div className='page-header'></div>
         <span>
           {this.state.wordData.nextWord}
         </span>
         <p>Your total score is: {this.state.wordData.totalScore}</p>
+        <div className='total-score'></div>
+
         <main>
-          <form>
-            <label htmlFor='learn-guess-input'>
+          <form className='answer-form'>
+            <label id='learn-guess-label' htmlFor='learn-guess-input'>
               What's the translation for this word?
             </label>
             <input id='learn-guess-input' type='text' required />
-            <button type='submit'>
+            <button className='submit-button' type='submit'>
               Submit your answer
             </button>
           </form>
           <section className='countBox'>
             <p>You have answered this word correctly {this.state.wordData.wordCorrectCount} times.</p>
             <p>You have answered this word incorrectly {this.state.wordData.wordIncorrectCount} times.</p>
+            <div className='page-header'></div>
           </section>
         </main>
       </section>
