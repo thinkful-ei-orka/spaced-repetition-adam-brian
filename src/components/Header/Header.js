7416642939290bb936364d0d1f93433f5d263a7b
Adam Brown <adam.edward.brown@gmail.com>
2020-07-31 15:11:14 -0400

    name change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -44,7 +44,7 @@ class Header extends Component {
       <header>
         <h1>
           <Link to='/'>
-            Spaced repetition
+            Rosetta repetition
           </Link>
         </h1>
         {TokenService.hasAuthToken()
